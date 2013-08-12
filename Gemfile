02bdc7a34ffe39cab55462c2a1bca17317a4633d
Marble Wu <huobazi@gmail.com>
2013-08-12 18:27:33 +0800

    up

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
+gem 'pg'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,9 @@ class ItemsController < ApplicationController
   def index
     @upload_token = Qiniu::RS.generate_upload_token :scope => 'spec-test',
       :callback_url       => items_url,
-      :callback_body      => 'a=1&key=$(etag)&size=$(fsize)&uid=$(endUser)',
-      :customer           => ''
+      :callback_body_type   => 'application/x-www-form-urlencoded',
+      :callback_body => 'foo=bar&w=$(imageInfo.width)&h=$(imageInfo.height)&exif=$(exif)&u=$(endUser)',
+      :customer           =>'xxx'
   end
 
   def create
