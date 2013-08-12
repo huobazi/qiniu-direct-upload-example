f67acd42feed5bb9671e7f01b518fe8be4eba635
Marble Wu <huobazi@gmail.com>
2013-08-12 19:00:57 +0800

    x

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,10 @@ class ItemsController < ApplicationController
   def index
     @upload_token = Qiniu::RS.generate_upload_token :scope => 'spec-test',
       :callback_url       => items_url,
+      :escape => 1,
+      :callback_body => 'w=$(imageInfo.width)&h=$(imageInfo.height)&exif=$(exif)&u=$(endUser)&custom_field_1=$(x:custom_field_1)',
       :callback_body_type   => 'application/x-www-form-urlencoded',
-      :callback_body => 'foo=bar&w=$(imageInfo.width)&h=$(imageInfo.height)&exif=$(exif)&u=$(endUser)',
-      :customer           =>'xxx'
+      :customer           =>'huobazi'
   end
 
   def create
