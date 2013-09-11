bd399420efe2696154df221282be308fc398fe7e
Marble Wu <huobazi@gmail.com>
2013-09-12 00:03:00 +0800

    change key

--- config/initializers/tmp.rb
+++ config/initializers/tmp.rb
@@ -1,2 +1,2 @@
-Qiniu::RS.establish_connection! :access_key => "QXaS_hTivIf74lUlMLhS4EGkobUU-arZ_Za5LZPH",
-                                :secret_key => "yfsXHe-dz3sJ2MGk9FwNJDImgMVCIKlSXXISNGXJ"
+Qiniu::RS.establish_connection! :access_key => "your key",
+                                :secret_key => "your key"
