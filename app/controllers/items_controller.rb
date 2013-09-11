60f879a0b8f5b5f5a7ac3f42d54b594f0a45c451
Marble Wu <huobazi@gmail.com>
2013-09-12 00:53:52 +0800

    add qiniu setting example file

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
-
   def create
     # TODO save the item, you can use a active_record object
     item                 = {}
+
     item[:id]            = 1
     item[:file_size]     = params[:fsize]
     item[:image]         = 'http://your-bucket-name.qiniudn.com' + params[:key]
     item[:imageInfo]     = params[:imageInfo]
     item[:custom_fields] = params[:custom_fields]
+    item[:xyz]           = params[:xyz]
 
     render json: item
   end
-
 end
